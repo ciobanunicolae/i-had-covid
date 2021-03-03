581f0eedb5df9d6af4d5c76dbc45b6cfdf259a7b
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-03-03 19:31:03 +0200

    Server modified and final.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
+//Requirements
 const express = require("express");
 const mongoose = require("mongoose");
-const app = use(express);
+//Routes
+const routes = require("./routes/api");
 
+//App config
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/db", routes);
+
+//Mongo config
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Listener
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
