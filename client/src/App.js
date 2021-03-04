0a955e2dd388033a909b0811f5e32b12ab2c7f70
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-03-04 09:19:03 +0200

    Included Home and NotFound route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+
+const Wrapper = styled.div`
+max-width: 1000px;
+color : red;
+align-items: center;
+margin: 0 auto;
+`
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Title</h1>
-    </div>
+        <Navbar />
+        <Wrapper>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+          </Switch>
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
